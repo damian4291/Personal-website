a557910ded1fae5290f19e8967b6e487b767f15c
Damian Zawadzinski <dzawadzinski@Damians-MacBook-Pro.local>
2016-03-09 17:13:37 +0100

    beginning of something

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+// IIFE - Immediately Invoked Function Expression
+(function($, window, document) {
+
+    $(function() {
+
+    });
+
+}(window.jQuery, window, document));
